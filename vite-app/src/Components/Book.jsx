d19ff5e680cf844b5d47865d4212547a5fddb6ff
bkleinb78 <y2bk728@aol.com>
2024-01-02 18:00:07 -0600

    Updated book.jsx with adding the book cover

--- vite-app/src/Components/Book.jsx
+++ vite-app/src/Components/Book.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
-
+import bookCover from './bookcover.jpg'
 const Book = () => {
   return (
-    <div>Book</div>
-  )
-}
+    <div className="book">
+    <img src={bookCover} alt="Book Cover" className="w-full max-w-sm rounded shadow-lg"/>
+    
+    <h1>Book Title</h1>
+    
+    <p>Book content...</p>
+  
+  </div>
+  );
+};
 
 export default Book
